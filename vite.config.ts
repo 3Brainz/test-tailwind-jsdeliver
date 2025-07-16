ca2a288ad54f9b1f6e416cef0f1f849bbad4e373
paolo_roberto_punzo <robertopunzo97@icloud.com>
2025-07-16 11:50:36 +0200

    tes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+    assetsDir: ".",
+    rollupOptions: {
+      output: {
+        // evita di generare chunks in sotto-cartelle tipo "assets"
+        chunkFileNames: "[name].[hash].js",
+        entryFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash].[ext]",
+      },
+    },
+  },
   plugins: [react()],
-})
+});
