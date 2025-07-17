bae8e0bcf6bbcd43a4bf9de8cf71830922efdac9
paolo_roberto_punzo <robertopunzo97@icloud.com>
2025-07-17 10:52:03 +0200

    hello

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist",
-    assetsDir: ".",
-    rollupOptions: {
-      output: {
-        // evita di generare chunks in sotto-cartelle tipo "assets"
-        chunkFileNames: "[name].[hash].js",
-        entryFileNames: "[name].[hash].js",
-        assetFileNames: "[name].[hash].[ext]",
-      },
-    },
-  },
-  plugins: [react()],
+  // build: {
+  //   outDir: "dist",
+  //   assetsDir: ".",
+  //   rollupOptions: {
+  //     output: {
+  //       // evita di generare chunks in sotto-cartelle tipo "assets"
+  //       chunkFileNames: "[name].[hash].js",
+  //       entryFileNames: "[name].[hash].js",
+  //       assetFileNames: "[name].[hash].[ext]",
+  //     },
+  //   },
+  // },
+  plugins: [react(), tailwindcss()],
 });
